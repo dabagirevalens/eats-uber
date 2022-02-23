f7fac4065743c85056f038e45a3de188dcbbc74c
dabagirevalens <vdabagire@gmail.com>
2022-02-23 16:31:14 +0200

    search bar

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,14 @@
 import { View, SafeAreaView } from 'react-native'
 import React from 'react'
 import HeaderTabs from '../components/HeaderTabs'
+import SearchBar from '../components/SearchBar'
 
 const Home = () => {
     return (
-        <SafeAreaView style={{ backgroundColor: '#eee', flex: 1 }}>
-            <View style={{ backgroundColor: 'white', padding: 15 }}>
+        <SafeAreaView style={{ width: '100%', height: '100%', backgroundColor: '#eee' }}>
+            <View style={{ backgroundColor : 'white', padding: 15}}>
                 <HeaderTabs />
+                <SearchBar />
             </View>
         </SafeAreaView>
     )
