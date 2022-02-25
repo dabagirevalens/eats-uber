1b84e3f6f36aee53b89b1ebbc518d0885368f199
dabagirevalens <vdabagire@gmail.com>
2022-02-25 21:10:30 +0200

    restaurants data

--- screens/Home.js
+++ screens/Home.js
@@ -3,9 +3,12 @@ import React from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import SearchBar from '../components/SearchBar'
 import Categories from '../components/Categories'
-import RestaurantItem from '../components/RestaurantItem'
+import RestaurantItems, { localRestaurants } from '../components/RestaurantItems'
 
 const Home = () => {
+
+    const [restaurantsData, setRestaurantsData] = React.useState(localRestaurants)
+
     return (
         <SafeAreaView style={{ width: '100%', height: '100%', backgroundColor: '#eee' }}>
             <View style={{ backgroundColor: 'white', padding: 15 }}>
@@ -14,10 +17,7 @@ const Home = () => {
             </View>
             <ScrollView showsVerticalScrollIndicator={false}>
                 <Categories />
-                <RestaurantItem />
-                <RestaurantItem />
-                <RestaurantItem />
-                <RestaurantItem />
+                <RestaurantItems restaurantsData={restaurantsData} />
             </ScrollView>
         </SafeAreaView>
     )
